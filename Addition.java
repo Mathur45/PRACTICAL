1bfdf42e7ebf03821aeb15ac3dcdc8a4553a8b5e
Mathur45 <110521410+Mathur45@users.noreply.github.com>
2022-08-10 16:49:13 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+class Addition
+{
+public static void main(String []args)
+{
+System.out.println("The Numbers are : ");
+int a = 10;
+int b = 20;
+int c = 30;
+System.out.println(a + "," + b + "and" + c);
+int Add = a+b+c;
+System.out.println(" The Sum of the Numbers is ;" + Add);
+}}

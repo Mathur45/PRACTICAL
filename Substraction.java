7a8e3ff5353f5c1b13cdcbe78262b917c2c39ba9
Mathur45 <110521410+Mathur45@users.noreply.github.com>
2022-08-10 16:49:38 +0530

    Add files via upload

---
+++ Substraction.java
@@ -0,0 +1,11 @@
+class Substraction
+{
+public static void main(String []args)
+{
+System.out.println("The Numbers are : ");
+int a = 20;
+int b = 10;
+System.out.println(a + "," + b);
+int Substraction = a-b;
+System.out.println(" The Substraction of the Numbers is :" + Substraction);
+}}

ab4a71cd2e15b832011dcf601d29422a95645191
Mathur45 <110521410+Mathur45@users.noreply.github.com>
2022-08-10 17:02:56 +0530

    Add files via upload

---
+++ Rectanglearea.java
@@ -0,0 +1,14 @@
+class Rectanglearea
+{
+public static void main(String []args)
+{
+System.out.println("The Length of Rectangle:");
+int a = 10;
+System.out.println(a);
+System.out.println("The Width of Rectangle:");
+int b = 7;
+System.out.println(b);
+int Area = (a*b);
+System.out.println("The Area of Rectangle is :" + Area);
+}
+}

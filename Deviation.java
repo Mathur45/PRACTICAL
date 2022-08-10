67880b6d3bb000e2f83369277e8ea1d5385ec47b
Mathur45 <110521410+Mathur45@users.noreply.github.com>
2022-08-10 16:50:25 +0530

    Add files via upload

---
+++ Deviation.java
@@ -0,0 +1,11 @@
+class Deviation
+{
+public static void main(String []args)
+{
+System.out.println("The Numbers are : ");
+int a = 20;
+int b = 10;
+System.out.println(a + "," + b);
+int Deviation = a/b;
+System.out.println(" The Deviation of the Numbers is :" + Deviation);
+}}

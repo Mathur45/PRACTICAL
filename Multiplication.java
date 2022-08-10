1576d3a5b5483b168c49b240e785c4d53da4f900
Mathur45 <110521410+Mathur45@users.noreply.github.com>
2022-08-10 16:50:03 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+class Multiplication
+{
+public static void main(String []args)
+{
+System.out.println("The Numbers are : ");
+int a = 20;
+int b = 10;
+System.out.println(a + "," + b);
+int Multiplication = a*b;
+System.out.println(" The Multiplication of the Numbers is :" + Multiplication);
+}}
